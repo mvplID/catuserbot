ec95497d8da0b7339c2d6e0453f4dd2e90a14a84
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-12-09 16:41:10 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -20,13 +20,9 @@ from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 from . import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
 from .helpers.exceptions import CancelProcess
 
-ENV = bool(os.environ.get("ENV", False))
 
-if ENV:
-    from .Config import Config
-else:
-    if os.path.exists("config.py"):
-        from config import Development as Config
+from userbot.Config import Config
+
 
 
 def load_module(shortname):
@@ -45,7 +41,7 @@ def load_module(shortname):
         import userbot.utils
 
         from .helpers.utils import install_pip
-
+        from .managers import edit_delete , edit_or_reply
         path = Path(f"userbot/plugins/{shortname}.py")
         name = "userbot.plugins.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
@@ -212,93 +208,7 @@ def sudo_cmd(pattern=None, command=None, **args):
     return events.NewMessage(**args)
 
 
-# https://t.me/c/1220993104/623253
-# https://docs.telethon.dev/en/latest/misc/changelog.html#breaking-changes
-async def edit_or_reply(
-    event,
-    text,
-    parse_mode=None,
-    link_preview=None,
-    file_name=None,
-    aslink=False,
-    linktext=None,
-    caption=None,
-):
-    link_preview = link_preview or False
-    reply_to = await event.get_reply_message()
-    if len(text) < 4096:
-        parse_mode = parse_mode or "md"
-        if event.sender_id in Config.SUDO_USERS:
-            if reply_to:
-                return await reply_to.reply(
-                    text, parse_mode=parse_mode, link_preview=link_preview
-                )
-            return await event.reply(
-                text, parse_mode=parse_mode, link_preview=link_preview
-            )
-        return await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
-    asciich = ["*", "`", "_"]
-    for i in asciich:
-        text = re.sub(rf"\{i}", "", text)
-    if aslink:
-        linktext = linktext or "Message was to big so pasted to bin"
-        try:
-            key = (
-                requests.post(
-                    "https://nekobin.com/api/documents", json={"content": text}
-                )
-                .json()
-                .get("result")
-                .get("key")
-            )
-            text = linktext + f" [here](https://nekobin.com/{key})"
-        except:
-            text = re.sub(r"•", ">>", text)
-            kresult = requests.post(
-                "https://del.dog/documents", data=text.encode("UTF-8")
-            ).json()
-            text = linktext + f" [here](https://del.dog/{kresult['key']})"
-        if event.sender_id in Config.SUDO_USERS:
-            if reply_to:
-                return await reply_to.reply(text, link_preview=link_preview)
-            return await event.reply(text, link_preview=link_preview)
-        return await event.edit(text, link_preview=link_preview)
-    file_name = file_name or "output.txt"
-    caption = caption or None
-    with open(file_name, "w+") as output:
-        output.write(text)
-    if reply_to:
-        await reply_to.reply(caption, file=file_name)
-        await event.delete()
-        return os.remove(file_name)
-    if event.sender_id in Config.SUDO_USERS:
-        await event.reply(caption, file=file_name)
-        await event.delete()
-        return os.remove(file_name)
-    await event.client.send_file(event.chat_id, file_name, caption=caption)
-    await event.delete()
-    os.remove(file_name)
 
-
-async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None):
-    parse_mode = parse_mode or "md"
-    link_preview = link_preview or False
-    time = time or 5
-    if event.sender_id in Config.SUDO_USERS:
-        reply_to = await event.get_reply_message()
-        catevent = (
-            await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode)
-            if reply_to
-            else await event.reply(
-                text, link_preview=link_preview, parse_mode=parse_mode
-            )
-        )
-    else:
-        catevent = await event.edit(
-            text, link_preview=link_preview, parse_mode=parse_mode
-        )
-    await asyncio.sleep(time)
-    return await catevent.delete()
 
 
 def errors_handler(func):
@@ -354,7 +264,7 @@ def errors_handler(func):
 
 
 async def progress(
-    current, total, event, start, type_of_ps, file_name=None, is_cancelled=None
+        current, total, event, start, type_of_ps, file_name=None, is_cancelled=None
 ):
     """Generic progress_callback for uploads and downloads."""
     now = time.time()
@@ -424,11 +334,11 @@ def time_formatter(milliseconds: int) -> str:
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
     tmp = (
-        ((str(days) + " day(s), ") if days else "")
-        + ((str(hours) + " hour(s), ") if hours else "")
-        + ((str(minutes) + " minute(s), ") if minutes else "")
-        + ((str(seconds) + " second(s), ") if seconds else "")
-        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+            ((str(days) + " day(s), ") if days else "")
+            + ((str(hours) + " hour(s), ") if hours else "")
+            + ((str(minutes) + " minute(s), ") if minutes else "")
+            + ((str(seconds) + " second(s), ") if seconds else "")
+            + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
     )
     return tmp[:-2]
 
@@ -447,7 +357,7 @@ async def is_admin(client, chat_id, user_id):
         req_jo = await client(GetParticipantRequest(channel=chat_id, user_id=user_id))
         chat_participant = req_jo.participant
         if isinstance(
-            chat_participant, (ChannelParticipantCreator, ChannelParticipantAdmin)
+                chat_participant, (ChannelParticipantCreator, ChannelParticipantAdmin)
         ):
             return True
     except Exception:
