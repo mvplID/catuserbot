3b82531d1b47d60a6e970ac89a2d96fba6a1c735
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-12-13 11:42:10 +0000

    pylint: auto fixes

--- userbot/Config/catub_config.py
+++ userbot/Config/catub_config.py
@@ -5,7 +5,7 @@ from os import environ
 ENV = bool(environ.get("ENV", False))
 
 if ENV:
-    from sample_config import Config # noqa
+    from sample_config import Config  # noqa
 else:
     if os.path.exists("config.py"):
-        from config import Development as Config # noqa
+        from config import Development as Config  # noqa
