18f8568c7a0cd10855484f3749a77cdd65e80225
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-12-13 17:40:23 +0530

    Update and rename init/clone.sh to .github/clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -30,24 +30,24 @@ if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
     rm -rf userbot
-    rm -rf init
+    rm -rf .github
     rm -rf sample_config.py
     rm -rf requirements.txt
     git clone https://github.com/sandy1709/catuserbot cat_ub
     mv cat_ub/userbot .
-    mv cat_ub/init . 
+    mv cat_ub/.github . 
     mv cat_ub/.git .
     mv cat_ub/sample_config.py .
     mv cat_ub/requirements.txt .
     rm -rf cat_ub
-    python ./init/update.py
+    python ./.github/update.py
 fi
 
 FILE=/app/bin/
 if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
-    bash ./init/bins.sh
+    bash ./.github/bins.sh
 fi
 
 python -m userbot
