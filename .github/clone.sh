6dd2170fd7b86de5b646a3590503ef31ee8c3797
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-12-13 17:55:53 +0530

    Update clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -33,7 +33,7 @@ else
     rm -rf .github
     rm -rf sample_config.py
     rm -rf requirements.txt
-    git clone https://github.com/sandy1709/catuserbot cat_ub
+    git clone -b bugs https://github.com/sandy1709/catuserbot cat_ub
     mv cat_ub/userbot .
     mv cat_ub/.github . 
     mv cat_ub/.git .
