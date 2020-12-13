e0bcc0ba5cb26f5aa7457d5b6fc48dc09d032e44
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-12-13 17:29:29 +0530

    Update clone.sh

--- init/clone.sh
+++ init/clone.sh
@@ -33,14 +33,14 @@ else
     git clone -b bugs https://github.com/sandy1709/catuserbot cat_ub
     mv cat_ub/* ../
     rm -rf cat_ub
-    python ./init/update.py
+    python init/update.py
 fi
 
 FILE=/app/bin/
 if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
-    bash ./init/bins.sh
+    bash init/bins.sh
 fi
 
 python -m userbot
